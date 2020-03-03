68b42beafebfeb75fce771a56ffe2c1d9309e7a9
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2020-03-03 14:21:44 -0800

    Trying to see whether .init_array actually works.

--- src/flb_plugin.c
+++ src/flb_plugin.c
@@ -200,7 +200,7 @@ int flb_plugin_load(char *path, struct flb_plugins *ctx,
     struct flb_plugin *plugin;
     struct flb_input_plugin *input;
     struct flb_filter_plugin *filter;
-    struct simple_s *output;
+    int *output;
 
     /* Open the shared object file: dlopen(3) */
     dso_handle = get_handle(path);
@@ -244,9 +244,10 @@ int flb_plugin_load(char *path, struct flb_plugins *ctx,
     }
     else if (is_output(plugin_stname) == FLB_TRUE) {
         type = FLB_PLUGIN_OUTPUT;
-        output = (struct simple_s *) symbol;
-        fprintf(stderr, "In C type: %d\n", output->type);
-        fprintf(stderr, "In C flags: %d\n", output->flags);
+        output = (struct int *) symbol;
+        fprintf(stderr, "In C type: %d\n", *output);
+        // fprintf(stderr, "In C type: %d\n", output->type);
+        // fprintf(stderr, "In C flags: %d\n", output->flags);
         fflush(stderr);
         // mk_list_add(&output->_head, &config->out_plugins);
     }
