392febc12d6ac76fe47f9a4888c3e6036baafd39
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2020-03-05 14:47:10 -0800

    Yea seems to be a problem when passed the struct into a Rust function

--- src/flb_output.c
+++ src/flb_output.c
@@ -678,7 +678,7 @@ int flb_output_init_all(struct flb_config *config)
         fprintf(stderr, "right before cb_init, ins->properties.prev: %p\n", ins->properties.prev);
         fprintf(stderr, "right before cb_init, ins->properties.next: %p\n", ins->properties.next);
         fprintf(stderr, "right before cb_init, ins->match: %p\n", ins->match);
-        ret = check_it(ins, config, ins->data);
+        ret = p->cb_init(ins, config, ins->data);
         fprintf(stderr, "right after cb_init, ins->config_map 3rd: %p\n", ins->config_map); 
         mk_list_init(&ins->th_queue);
         if (ret == -1) {
