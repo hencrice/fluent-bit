0aa3fbf3177101a008014a1f7a5c8eaa9080b22c
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2020-03-06 10:52:15 -0800

    I was being dumb. The Rust plugin uses a statically compiled fluent-bit library. So I need to recompile that instead of compiling the fluent-bit executable!!!

--- src/flb_output.c
+++ src/flb_output.c
@@ -371,10 +371,9 @@ static inline int prop_key_check(const char *key, const char *kv, int k_len)
 #include <unistd.h>
 
 void flb_output_return_no_inline(int x) {
-    printf("called in flb_output_return_no_inline\n");
-    fflush(stdout);
-    sleep(5);
-    // flb_output_return_do(x);
+    fprintf(stderr, "called in flb_output_return_no_inline\n");
+    fflush(stderr);
+    flb_output_return_do(x);                                       
 }
 
 /* Override a configuration property for the given input_instance plugin */
