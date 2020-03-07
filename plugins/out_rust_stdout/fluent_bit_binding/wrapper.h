e1ae5f4c2a2fc4dec466f41e53485fa49a560a8e
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2020-03-06 23:41:42 -0800

    Try these

--- plugins/out_rust_stdout/fluent_bit_binding/wrapper.h
+++ plugins/out_rust_stdout/fluent_bit_binding/wrapper.h
@@ -3,3 +3,7 @@
 #include <fluent-bit/flb_output.h>
 #include <fluent-bit/flb_pack.h>
 #include <fluent-bit/flb_config_map.h>
+
+// tomorrow try:
+// https://stackoverflow.com/questions/42741815/setting-the-include-path-with-bindgen
+// https://stackoverflow.com/questions/9985315/cmake-including-h-files-from-other-directories
