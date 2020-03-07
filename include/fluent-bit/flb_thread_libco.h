ebcbe1d3bcc3f8ccdf0f877db2a6b460139c7cbe
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2020-03-06 16:41:56 -0800

    Moved the location of the print statement to after pthread_key_create() call

--- include/fluent-bit/flb_thread_libco.h
+++ include/fluent-bit/flb_thread_libco.h
@@ -68,8 +68,8 @@ FLB_EXPORT pthread_key_t flb_thread_key;
 #include <stdio.h>
 static FLB_INLINE void flb_thread_prepare(void)
 {
-    fprintf(stderr, "flb_thread_prepare flb_thread_key: %d\n", flb_thread_key);
     pthread_key_create(&flb_thread_key, NULL);
+    fprintf(stderr, "flb_thread_prepare flb_thread_key: %d\n", flb_thread_key);
 }
 
 static FLB_INLINE void flb_thread_yield(struct flb_thread *th, int ended)
