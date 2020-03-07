7e8a5267e102d3d1305c5107d6dd52ef92a87169
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2020-03-07 14:24:04 -0800

    Will this work? Seems like subsequent calls to `println!("cargo:rustc-link-search=native={:?}"` will overwrite the previous call.

--- plugins/out_rust_stdout/fluent_bit_out_std/build.rs
+++ plugins/out_rust_stdout/fluent_bit_out_std/build.rs
@@ -12,24 +12,25 @@ fn main() {
     let library_search_path = env::var_os("CMAKE_LIBRARY_OUTPUT_DIRECTORY").expect("CMAKE_LIBRARY_OUTPUT_DIRECTORY not provided");
     println!("archive_search_path: {:?}", archive_search_path);
     println!("library_search_path: {:?}", library_search_path);
-    println!("cargo:rustc-link-search=native={:?}", archive_search_path);
-    // println!("cargo:rustc-link-search=native={:?}", library_search_path);
-
+    
     // Specify static libraries to link
-    println!("cargo:rustc-link-lib=static=fluent-bitd"); // libfluent-bit.a
+    println!("cargo:rustc-link-search=native={:?}", archive_search_path);
+    println!("cargo:rustc-link-lib=static=fluent-bit"); // libfluent-bit.a
     println!("cargo:rustc-link-lib=static=mk_core"); // libmk_core.a
     println!("cargo:rustc-link-lib=static=co"); // libco.a
     println!("cargo:rustc-link-lib=static=mpack-static"); // libmpack-static.a
     println!("cargo:rustc-link-lib=static=msgpackc"); // libmsgpackc.a
     println!("cargo:rustc-link-lib=static=jsmn"); // libjsmn.a
     println!("cargo:rustc-link-lib=static=chunkio-static"); // libchunkio-static.a
-    println!("cargo:rustc-link-lib=static=onigmo"); // libonigmo.a
     println!("cargo:rustc-link-lib=static=cio-crc32"); //libcio-crc32.a
-
     // not sure whether these are needed. Might as wll write a for loop
     // that looks for all the .a files under /usr/local/lib and link
     // all of them?
     println!("cargo:rustc-link-lib=static=tutf8e"); // libtutf8e.a
     println!("cargo:rust-link-lib=static=miniz"); // libminiz.a
     println!("cargo:rust-link-lib=static=rbtree"); // librbtree.a
+
+    // this static library is located under a different path
+    println!("cargo:rustc-link-search=native={:?}", library_search_path);
+    println!("cargo:rustc-link-lib=static=onigmo"); // libonigmo.a
 }
