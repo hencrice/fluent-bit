691dec447da2591ded044fe0575664610e3c9978
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2020-03-07 12:13:55 -0800

    Fixed compilation issue

--- plugins/out_rust_stdout/fluent_bit_binding/build.rs
+++ plugins/out_rust_stdout/fluent_bit_binding/build.rs
@@ -51,13 +51,13 @@ fn main() {
 
     let bindgen_include_dirs = env::var_os("BINDGEN_HEADER_DIRS")
         .expect("BINDGEN_HEADER_DIRS not provided");
-    println!("BINDGEN_HEADER_DIRS: {:?}", bindgen_includes);
-    let clang_args = bindgen_includes.into_string()
+    println!("BINDGEN_HEADER_DIRS: {:?}", bindgen_include_dirs);
+    let clang_args = bindgen_include_dirs.into_string()
         .expect("can not convert BINDGEN_HEADER_DIRS into String")
         .split_ascii_whitespace();
     for arg in clang_args {
         // https://stackoverflow.com/questions/42741815/setting-the-include-path-with-bindgen
-        binding_builder.clang_arg(arg)
+        binding_builder.clang_arg(arg);
     }    
 
     // Finish the builder and generate the bindings.
@@ -67,7 +67,7 @@ fn main() {
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
+    binding
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
