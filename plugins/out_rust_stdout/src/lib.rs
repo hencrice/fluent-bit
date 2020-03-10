8c1f6c27605acd78b51f0e92ff021868ff38d009
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2020-03-10 16:11:00 -0700

    Fixing

--- plugins/out_rust_stdout/src/lib.rs
+++ plugins/out_rust_stdout/src/lib.rs
@@ -299,7 +299,7 @@ async fn delay_rand_u8() -> u8 {
 
 // A future that can reschedule itself to be polled by an `Executor`
 // (in fleunt-bit's case, it's the event loop in the fluent-bit core).
-struct NoOp{};
+struct NoOp;
 
 impl ArcWake for NoOp {
     // Wakers are responsible for scheduling a task to be polled again
@@ -414,9 +414,8 @@ pub <TodoOutputType> fn ExecuteFuture(todo: &mut Future<TodoOutputType>, config:
                     } else {
                         break Err(CCallNonZeroError{ret}) 
                     }
-                }
             },
-        } 
+        }
     }
 }
 
