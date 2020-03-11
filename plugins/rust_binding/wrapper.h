ca34f4b68335b9361f0f78308131a92c8b627474
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2020-03-10 22:38:13 -0700

    Include the new header file. This commit successfully produced a running fluent-bit executable. However, I ran into:
    fluentbit_1  | [2020/03/11 05:59:07] [debug] [task] created task=0x5a88d70 id=0 OK
    fluentbit_1  |     timestamp: 1583906346,
    fluentbit_1  |     record: {
    fluentbit_1  |         "container_id": "8100262a69178ee36c15f762fa41c8482857083d6afd2d67b9d9b6f4b15c1c51",
    fluentbit_1  |         "container_name": "/test_nginx_1",
    fluentbit_1  |         "source": "stdout",
    fluentbit_1  |         "log": "172.18.0.1 - - [11/Mar/2020:05:59:06 +0000] \"GET / HTTP/1.1\" 304 0 \"-\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36\" \"-\"",
    fluentbit_1  |     },
    fluentbit_1  | }
    fluentbit_1  | delay_rand_u8 called, before async sleep
    fluentbit_1  | [2020/03/11 05:59:07] [  Error] epoll_ctl: Bad file descriptor, errno=9 at /usr/src/myapp/fluent-bit/lib/monkey/mk_core/mk_event_epoll.c:136
    
    This is mainly because in Rust, most of the libraries that expose async operations as Future, probably do not provide explicit access to the underlying low-level OS async primitives, such as epoll on file descriptors. This means that we can not correctly call mk_event_add with the right fd, which resulted in this error.

--- plugins/rust_binding/wrapper.h
+++ plugins/rust_binding/wrapper.h
@@ -1,6 +1,6 @@
+#include <fluent-bit/flb_plugin.h>
 #include <fluent-bit/flb_input.h>
 #include <fluent-bit/flb_filter.h>
 #include <fluent-bit/flb_output.h>
 #include <fluent-bit/flb_pack.h>
 #include <fluent-bit/flb_config_map.h>
-#include <fluent-bit/flb_thread_libco.h>
